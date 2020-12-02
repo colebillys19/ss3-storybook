b5d5c789e73b4e8e833d13245c390c9fad9dffec
Billy Cole <cole.billys@gmail.com>
2020-12-01 17:13:21 -0800

    remove unused imports

--- src/stories/Input/PaymentTextField.stories.js
+++ src/stories/Input/PaymentTextField.stories.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-//
-import aaa from '../../utils/svg/BaseUI-Icons/PaymentTextField/CheckMark-24x24.svg';
-import bbb from '../../utils/svg/BaseUI-Icons/PaymentTextField/Unchecked-24x24.svg';
-
 import PaymentTextField from '../../components/Input/PaymentTextField';
 
 /* eslint-disable import/no-anonymous-default-export */
